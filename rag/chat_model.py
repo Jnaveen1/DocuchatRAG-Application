1d54de58c2b1830088fd88040a0d14d691439571
naveenJV <naveenjanapati65@gmail.com>
2026-06-24 09:02:50 +0530

    updated chat model

--- rag/chat_model.py
+++ rag/chat_model.py
@@ -1,13 +1,8 @@
-from langchain.chat_models import init_chat_model
+from langchain_google_genai import ChatGoogleGenerativeAI
 from config import GOOGLE_API_KEY
 
-print("Google API Key:", GOOGLE_API_KEY)
-
 def get_llm():
-
-    model = init_chat_model(
-        "google_genai:gemini-2.5-flash",
-        api_key=GOOGLE_API_KEY
+    return ChatGoogleGenerativeAI(
+        model="gemini-2.5-flash",
+        google_api_key=GOOGLE_API_KEY
     )
-
-    return model
