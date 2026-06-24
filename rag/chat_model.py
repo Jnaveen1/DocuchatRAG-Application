6a06f22e3301a84228ab1a9edcc51c5b172fa40a
naveenJV <naveenjanapati65@gmail.com>
2026-06-24 09:22:15 +0530

    update requirements.txt and chat model

--- rag/chat_model.py
+++ rag/chat_model.py
@@ -3,6 +3,6 @@ from config import GOOGLE_API_KEY
 
 def get_llm():
     return ChatGoogleGenerativeAI(
-        model="gemini-1.5-flash",
+        model="gemini-2.5-flash",
         google_api_key=GOOGLE_API_KEY
     )
